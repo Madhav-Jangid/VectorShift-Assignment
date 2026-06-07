ab6d0e5af0affc80857bda9406e5c2e55950ab31
Madhav Jangid <manumadhavjangid@gmail.com>
2026-06-07 18:15:35 +0530

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './resizeObserverFix';
 import './styles/index.css';
 import App from './app/App';
 
--- frontend/src/services/pipelineApi.js
+++ frontend/src/services/pipelineApi.js
@@ -1,7 +1,15 @@
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL?.replace(/\/+$/, '');
+
+function getPipelineUrl(path) {
+  if (!API_BASE_URL) {
+    throw new Error('Missing REACT_APP_BACKEND_URL environment variable');
+  }
+
+  return `${API_BASE_URL}${path}`;
+}
 
 export async function submitPipeline(nodes, edges) {
-  const response = await fetch(`${API_BASE_URL}/pipelines/parse`, {
+  const response = await fetch(getPipelineUrl('/pipelines/parse'), {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ nodes, edges }),
@@ -16,7 +24,7 @@ export async function submitPipeline(nodes, edges) {
 }
 
 export async function executePipeline(nodes, edges) {
-  const response = await fetch(`${API_BASE_URL}/pipelines/execute`, {
+  const response = await fetch(getPipelineUrl('/pipelines/execute'), {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ nodes, edges }),
